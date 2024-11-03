796fd3ad59f484223bd1050f79ed27e93af60ba3
Nitish Lobo <nitishlobo@gmail.com>
2024-11-04 06:06:03 +1000

    feat: add energy reading admin view

--- src/meter_readings/admin.py
+++ src/meter_readings/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.db.models import Model
 from django.http import HttpRequest
 
+from meter_readings.models.energy_readings import EnergyReading
 from meter_readings.models.flow_files import FlowFile, FlowFileMetadata
 
 
@@ -82,3 +83,34 @@ class FlowFileMetadataAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
 
     # Date hierarchy to make it easier to navigate by dates
     date_hierarchy = "file_created_at"
+
+
+@admin.register(EnergyReading)
+class EnergyReadingAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    """Admin view for EnergyReading."""
+
+    # Display these fields in the list view
+    list_display = (
+        # Primary focus fields
+        "flow_file",
+        "mpan_core",
+        "meter_id",
+        "register_reading",
+        "reading_at",
+        # Additional fields
+        "bsc_validation_status",
+        "meter_reading_type",
+        "meter_register_id",
+        "reading_method",
+        "meter_reading_flag",
+        "md_reset_at",
+        "number_of_md_resets",
+        "meter_reading_validation_result_status",
+        "meter_reading_validation_result_reason",
+        "mpan_site_visit_reason",
+        "meter_reading_site_visit_reason",
+        "register_reading_site_visit_reason",
+    )
+
+    # Fields that can be searched
+    search_fields = ("mpan_core", "meter_id", "register_reading", "reading_at")
